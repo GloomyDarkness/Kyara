9850549af277196aad65e2ec7f3b6eabfb35a9d3
GloomyDarkness <miaaumiau748@gmail.com>
2022-07-10 00:56:03 -0300

    update in buttons

--- src/events/client/interactionCreate.js
+++ src/events/client/interactionCreate.js
@@ -1,5 +1,6 @@
 const Event = require('../../structures/Event')
 const delay = ms => new Promise(res => setTimeout(res, ms));
+const { guilds } = require("../../database/models/Models")
 
 module.exports = class extends Event {
     constructor(client) {
@@ -11,12 +12,80 @@ module.exports = class extends Event {
 
     run = async (interaction) => {
 
+        let server = await (guilds.findById(interaction.guild.id)) || new guilds({ _id: interaction.guild.id })
 
         if (interaction.isCommand()) {
             if (!interaction.guild) return
             const cmd = this.client.commands.find(c => c.name === interaction.commandName)
             if (cmd) {
                 cmd.run(interaction)
+            }
+        } else if (interaction.isButton()) {
+            switch (interaction.customId) {
+
+                case 'antibot-on':
+
+
+                    if (server?.antibot.activated === true) return interaction.update({}) && interaction.channel.send({ content: '<:erradissimo:800148953613205534> O sistema de anti-botjá está ativado' }).then(msg => {
+                        setTimeout(() => msg.delete(), 10000)
+                    })
+
+                    server.antibot.activated = true
+                    server.markModified('antibot')
+                    server.save()
+                    interaction.update({})
+                    interaction.channel.send({ content: '<a:concluido:795402598345474108> sistema de anti-botativo com `sucesso`.' }).then(msg => {
+                        setTimeout(() => msg.delete(), 10000)
+                    })
+
+                    break;
+
+                case 'antibot-off':
+
+                    if (server?.antibot.activated === false) return interaction.update({}) && interaction.channel.send({ content: '<:erradissimo:800148953613205534> O sistema de anti-botjá está desativado' }).then(msg => {
+                        setTimeout(() => msg.delete(), 10000)
+                    })
+
+                    server.antibot.activated = false
+                    server.markModified('antibot')
+                    server.save()
+                    interaction.update({})
+                    interaction.channel.send({ content: '<a:concluido:795402598345474108> sistema de anti-botdesativado com `sucesso`.' }).then(msg => {
+                        setTimeout(() => msg.delete(), 10000)
+                    })
+
+                    break;
+
+                case 'dias':
+
+                    if (server?.antibot.activated === false || !server.antibot.activated) return interaction.update({}) && interaction.channel.send({ content: '<:erradissimo:800148953613205534> O sistema de anti-botestá `desativado`, ative-o primeiro antes de definir a quantia de dias.' }).then(msg => {
+                        setTimeout(() => msg.delete(), 10000)
+                    })
+
+                    const filter = m => m.author.id === interaction.user.id
+
+                    interaction.update({})
+                    interaction.channel.send('Insira a quantia de dias que a pessoa tem que ter para entrar no servidor').then(msg => {
+                        setTimeout(() => msg.delete(), 15000)
+                    })
+
+                    const collector = interaction.channel.createMessageCollector({ filter, max: 1, time: 15000 })
+
+                    collector.on('collect', l => {
+                        console.log(l.content)
+
+                        if (!l.content.isNaN) console.log('n é numero')
+
+                        l.delete()
+
+                    })
+                    collector.on('end', (c, r) => {
+                        if (r === 'time') interaction.channel.send({ content: 'Tempo esgotado.' }).then(msg => {
+                            setTimeout(() => msg.delete(), 10000)
+                        })
+                    })
+
+                    break;
             }
         }
     }
--- src/subCommands/admin/proteção.js
+++ src/subCommands/admin/proteção.js
@@ -1,16 +1,31 @@
-const { MessageEmbed } = require('discord.js')
-const { guilds } = require("../../database/models/Models")
+const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js')
 
 module.exports = async (client, interaction) => {
 
-    let server = await (guilds.findById(interaction.guild.id)) || new guilds({ _id: interaction.guild.id })
+    const button = new MessageButton()
+        .setCustomId('antibot-on')
+        .setLabel('ativar')
+        .setStyle('SUCCESS')
+
+    const button2 = new MessageButton()
+        .setCustomId('antibot-off')
+        .setLabel('desativar')
+        .setStyle('DANGER')
+
+    const button3 = new MessageButton()
+        .setCustomId('dias')
+        .setLabel('dias')
+        .setStyle('PRIMARY')
+
+    let row = new MessageActionRow()
+        .addComponents(button, button2, button3)
 
     let embed = new MessageEmbed()
         .setTitle('Kyara Menu')
-        .setDescription(`\`•\` Olá, seja bem vindo ao menu de proteção da Kyara! <:winkk:795332180473544714>\nOfereço à você sistemas de proteção`)
+        .setDescription(`\`•\` Olá, seja bem vindo ao menu de \`proteção da Kyara!\` <:winkk:795332180473544714>\nOfereço à você o sistema de proteção de ANTI-BOTS\n\n\`\`\`Configure do jeito que você deseja!\`\`\`\n\nO sistema consiste em **você definir** quantos dias as contas devem ter para entrar em seu servidor, evitando por exemplo aquelas pessoa divulgando link no pv de seus membros!\n\n> Utilize os botões abaixo para fazer a configuração!`)
         .setImage('https://i.imgur.com/FfeUjSb.gif')
         .setColor('RED')
 
-    interaction.reply({ embeds: [embed] })
+    interaction.reply({ embeds: [embed], components: [row] })
 
-}
+}   
