1edf5516a3b544df871bcd7abb371c35a1eb754c
GloomyDarkness <miaaumiau748@gmail.com>
2022-07-10 01:29:11 -0300

    modificação no texto

--- src/subCommands/admin/proteção.js
+++ src/subCommands/admin/proteção.js
@@ -6,7 +6,7 @@ module.exports = async (client, interaction) => {
         .setCustomId('antibot-on')
         .setLabel('ativar')
         .setStyle('SUCCESS')
-        
+
     const button2 = new MessageButton()
         .setCustomId('antibot-off')
         .setLabel('desativar')
@@ -14,7 +14,7 @@ module.exports = async (client, interaction) => {
 
     const button3 = new MessageButton()
         .setCustomId('dias')
-        .setLabel('dias')
+        .setLabel('tempo')
         .setStyle('PRIMARY')
 
     let row = new MessageActionRow()
@@ -22,7 +22,7 @@ module.exports = async (client, interaction) => {
 
     let embed = new MessageEmbed()
         .setTitle('Kyara Menu')
-        .setDescription(`\`•\` Olá, seja bem vindo ao menu de \`proteção da Kyara!\` <:winkk:795332180473544714>\nOfereço à você o sistema de proteção de ANTI-BOTS\n\n\`\`\`Configure do jeito que você deseja!\`\`\`\n\nO sistema consiste em **você definir** quantos dias as contas devem ter para entrar em seu servidor, evitando por exemplo aquelas pessoa divulgando link no pv de seus membros!\n\n> Utilize os botões abaixo para fazer a configuração!`)
+        .setDescription(`\`•\` Olá, seja bem vindo ao menu de \`proteção da Kyara!\` <:winkk:795332180473544714>\nOfereço à você o sistema de proteção de ANTI-BOTS\n\n\`\`\`Configure do jeito que você deseja!\`\`\`\n\nO sistema consiste em **você definir** quantos dias as contas devem ter para entrar em seu servidor, evitando por exemplo aquelas pessoa divulgando link no pv de seus membros!\n\n> Utilize os botões abaixo para fazer a configuração!\n> <a:alerta:798597669462474793> o tempo tende em ser em dias! ex: 1, 2...`)
         .setImage('https://i.imgur.com/FfeUjSb.gif')
         .setColor('RED')
 
