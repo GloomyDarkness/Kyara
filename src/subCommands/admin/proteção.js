7e4c4631c30ec0638e7204deed6150be0443eeee
GloomyDarkness <miaaumiau748@gmail.com>
2022-07-10 01:48:02 -0300

    modf texto

--- src/subCommands/admin/proteção.js
+++ src/subCommands/admin/proteção.js
@@ -22,7 +22,7 @@ module.exports = async (client, interaction) => {
 
     let embed = new MessageEmbed()
         .setTitle('Kyara Menu')
-        .setDescription(`\`•\` Olá, seja bem vindo ao menu de \`proteção\` da Kyara! <:winkk:795332180473544714>\n\n <a:concluido:795402598345474108> Ofereço à você o sistema de proteção de \`Anti-Bot\`\n\n\`\`\`Configure do jeito que você deseja!\`\`\`\n\nO sistema consiste em **você definir** quantos dias as contas devem ter para entrar em seu servidor, evitando por exemplo aquelas pessoa divulgando link no pv de seus membros!\n\n> Utilize os botões abaixo para fazer a configuração!\n> <a:alerta:798597669462474793> o tempo tende em ser em dias! ex: 1, 2...`)
+        .setDescription(`\`•\` Olá, seja bem vindo ao menu de \`proteção\` da Kyara! <:winkk:795332180473544714>\n\n <a:concluido:795402598345474108> Ofereço à você o sistema de proteção de \`Anti-Bot\`\n\n\`\`\`Configure do jeito que você deseja!\`\`\`\n\nO sistema consiste em **você definir** quantos dias as contas devem ter para entrar em seu servidor, evitando por exemplo aquelas pessoa divulgando link no pv de seus membros!\n\n> Utilize os botões abaixo para fazer a configuração!\n> <a:setaaa:796075080542584912> o tempo tende em ser em dias! ex: 1, 2...`)
         .setImage('https://i.imgur.com/FfeUjSb.gif')
         .setColor('RED')
 
